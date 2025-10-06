2a13a2d59a5b354e2af464498adb00d7a092c2a3
Do Tien Dat <135575463+Datdo010905@users.noreply.github.com>
2025-10-06 08:34:54 -0700

    thêm Model KhuyenMai, ThanhToan

---
+++ Project_QuanLyBanLe/Models/KhuyenMai.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class KhuyenMai
+    {
+        public string MaKM { get; set; }
+        public string TenKM { get; set; }
+        public string MaSP { get; set; }
+        public DateTime NgayBD { get; set; }
+        public DateTime NgayKT { get; set; }
+    }
+}
