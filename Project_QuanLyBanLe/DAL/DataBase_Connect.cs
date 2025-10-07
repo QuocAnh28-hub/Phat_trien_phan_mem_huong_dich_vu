6a22226bd3d3a70202420de6090326dae6da7f76
Do Tien Dat <135575463+Datdo010905@users.noreply.github.com>
2025-10-07 01:11:59 -0700

    thêm DAL Database_Connect

---
+++ Project_QuanLyBanLe/DAL/DataBase_Connect.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace DAL
+{
+    public class DataBase_Connect
+    {
+        protected string strCon = @"Data Source = DESKTOP-UHPB8VT;
+                                    Initial Catalog = QUANLYBANLE;
+                                    Integrated Security = True;
+                                    TrustServerCertificate = True;";
+
+        SqlDataAdapter sqlAdap;
+        DataTable dt;
+
+        //phi kết nối
+        public DataTable GetDataTableFromSP(string spName, params SqlParameter[] parameters)
+        {
+            using (SqlConnection conn = new SqlConnection(strCon))
+            {
+                SqlCommand cmd = new SqlCommand(spName, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // Nếu có tham số thì thêm vào
+                if (parameters != null)
+                    cmd.Parameters.AddRange(parameters);
+                sqlAdap = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                sqlAdap.Fill(dt);
+                return dt;
+            }
+        }
+
+
+    }
+}
