aa7e95f3a04391c8889801c6c39f4630c28ca18a
Do Tien Dat <135575463+Datdo010905@users.noreply.github.com>
2025-10-07 02:08:14 -0700

    update NhaCungCap_DAL

--- Project_QuanLyBanLe/DAL/NhaCungCap_DAL.cs
+++ Project_QuanLyBanLe/DAL/NhaCungCap_DAL.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,18 @@ namespace DAL
 {
     public class NhaCungCap_DAL
     {
+        DataBase_Connect db = new DataBase_Connect();
+        public DataTable getAllNCC()
+        {
+            try
+            {
+                DataTable dt = db.GetDataTableFromSP("sp_GetNhaCungCap");
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi khi lấy danh sách: " + ex.Message);
+            }
+        }
     }
 }
