12d11063a6c236c1e8a8a883080a66d01ba3bd79
Quoc_Anh <dohuuquocanh21dk@gmail.com>
2025-10-07 14:44:55 +0700

    Tạo API Lấy DanhMuc và thêm DanhMuc mới kiểu cục bộ

--- Project_QuanLyBanLe/BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
+++ Project_QuanLyBanLe/BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1983ff50e578a509bf50dacc072789b785bd6c07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42fda30388c617437ef3b23aa8080ad46310de66")]
 [assembly: System.Reflection.AssemblyProductAttribute("BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
