75158102b028251e7638952aaee4dba43864cb7d
Do Tien Dat <135575463+Datdo010905@users.noreply.github.com>
2025-10-07 01:16:32 -0700

    update KhachHang_BLL

--- Project_QuanLyBanLe/BLL/KhachHang_BLL.cs
+++ Project_QuanLyBanLe/BLL/KhachHang_BLL.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL;
 
 namespace BLL
 {
     public class KhachHang_BLL
     {
+        KhachHang_DAL KH_DAL = new KhachHang_DAL();
+
+        public DataTable getAllKH()
+        {
+            return KH_DAL.getAllKH();
+        }
     }
 }
