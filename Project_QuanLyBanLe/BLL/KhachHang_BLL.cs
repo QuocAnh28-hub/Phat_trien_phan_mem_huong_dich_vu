f560add2cc5114bbb2c041ec7081902680213c06
Do Tien Dat <135575463+Datdo010905@users.noreply.github.com>
2025-10-07 09:54:20 -0700

    update lần 2 KhachHang_BLL

--- Project_QuanLyBanLe/BLL/KhachHang_BLL.cs
+++ Project_QuanLyBanLe/BLL/KhachHang_BLL.cs
@@ -16,5 +16,21 @@ namespace BLL
         {
             return KH_DAL.getAllKH();
         }
+        public DataTable GetByIdKH(string makh)
+        {
+            return KH_DAL.GetByIdKH(makh);
+        }
+        public DataTable DeleteByIdKH(string makh)
+        {
+            return KH_DAL.DeleteByIdKH(makh);
+        }
+        public DataTable UpdateByIdKH(Models.KhachHang kh)
+        {
+            return KH_DAL.UpdateByIdKH(kh);
+        }
+        public DataTable CreateKH(Models.KhachHang kh)
+        {
+            return KH_DAL.CreateKH(kh);
+        }
     }
 }
