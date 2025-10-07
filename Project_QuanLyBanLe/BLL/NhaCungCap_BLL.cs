a496fd548c2e361f1b328d1e47535f225bbb104f
Do Tien Dat <135575463+Datdo010905@users.noreply.github.com>
2025-10-07 02:09:20 -0700

    update NhaCungCap_BLL

--- Project_QuanLyBanLe/BLL/NhaCungCap_BLL.cs
+++ Project_QuanLyBanLe/BLL/NhaCungCap_BLL.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DAL;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,11 @@ namespace BLL
 {
     public class NhaCungCap_BLL
     {
+        NhaCungCap_DAL NCC_DAL = new NhaCungCap_DAL();
+
+        public DataTable getAllNCC()
+        {
+            return NCC_DAL.getAllNCC();
+        }
     }
 }
