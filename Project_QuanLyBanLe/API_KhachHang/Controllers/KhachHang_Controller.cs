4dc235254fcd969e3b9339c37d6dfce681d7243d
Do Tien Dat <135575463+Datdo010905@users.noreply.github.com>
2025-10-07 01:25:07 -0700

    update KhachHang_Controller

--- Project_QuanLyBanLe/API_KhachHang/Controllers/KhachHang_Controller.cs
+++ Project_QuanLyBanLe/API_KhachHang/Controllers/KhachHang_Controller.cs
@@ -1,1 +1,38 @@
+﻿using BLL;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+namespace API_KhachHang.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KhachHang_Controller : ControllerBase
+    {
+        KhachHang_BLL KH_BLL = new KhachHang_BLL();
 
+        [HttpGet]
+        public IActionResult getAllKH()
+        {
+            try
+            {
+                DataTable dt = KH_BLL.getAllKH();
+                var list = new List<object>();
+                foreach (DataRow row in dt.Rows)
+                {
+                    list.Add(new
+                    {
+                        MaKH = row["MaKH"],
+                        TenKH = row["TenKH"],
+                        SDT = row["SDT"],
+                        DiaChi = row["DiaChi"]
+                    });
+                }
+                return Ok(new { success = true, message = "Lấy danh sách khách thành công", data = list });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { success = false, message = "Lỗi: " + ex.Message });
+            }
+        }
+    }
+}
