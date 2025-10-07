1635c2afaec37f6a8a045891aaa42c4554365fb4
Do Tien Dat <135575463+Datdo010905@users.noreply.github.com>
2025-10-07 01:15:03 -0700

    update KhachHang_DAL

--- Project_QuanLyBanLe/DAL/KhachHang_DAL.cs
+++ Project_QuanLyBanLe/DAL/KhachHang_DAL.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace DAL
 {
     public class KhachHang_DAL
     {
+        DataBase_Connect db = new DataBase_Connect();
+        public DataTable getAllKH()
+        {
+            try
+            {          
+                DataTable dt = db.GetDataTableFromSP("sp_GetKhachHang");
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi khi lấy danh sách: " + ex.Message);
+            }
+        }
     }
 }
