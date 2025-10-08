bcf34ac3f64ef65cb3e91838952479216a08d6d7
Do Tien Dat <135575463+Datdo010905@users.noreply.github.com>
2025-10-07 23:28:44 -0700

    update lần 2 NhaCungCap_DAL

--- Project_QuanLyBanLe/DAL/NhaCungCap_DAL.cs
+++ Project_QuanLyBanLe/DAL/NhaCungCap_DAL.cs
@@ -12,6 +12,8 @@ namespace DAL
     public class NhaCungCap_DAL
     {
         DataBase_Connect db = new DataBase_Connect();
+
+
         public DataTable getAllNCC()
         {
             try
@@ -22,6 +24,68 @@ namespace DAL
             catch (Exception ex)
             {
                 throw new Exception("Lỗi khi lấy danh sách: " + ex.Message);
+            }
+        }
+        public DataTable GetById(string ma)
+        {
+            try
+            {
+                SqlParameter[] para = { new SqlParameter("@MANCC", ma) };
+                DataTable dt = db.GetDataTableFromSP("sp_GetByIDNhaCungCap", para);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi khi lấy: " + ex.Message);
+            }
+        }
+
+        public DataTable Create(Models.NhaCungCap model)
+        {
+            try
+            {
+                SqlParameter[] parameters = { new SqlParameter("@MANCC", model.MaNCC.Trim()),
+                                             new SqlParameter("@TENNCC",  model.TenNCC.Trim()),
+                                             new SqlParameter("@DIACHI",  model.DiaChi.Trim()),
+                                                new SqlParameter("@SDT",  model.SDT.Trim()),
+                                                new SqlParameter("@EMAIL",  model.EMAIL.Trim())};
+                DataTable dt = db.GetDataTableFromSP("SP_THEMNCC", parameters);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi khi thêm: " + ex.Message);
+            }
+        }
+
+        public DataTable Update(Models.NhaCungCap model)
+        {
+            try
+            {
+                SqlParameter[] parameters = { new SqlParameter("@MANCC", model.MaNCC.Trim()),
+                                             new SqlParameter("@TENNCC",  model.TenNCC.Trim()),
+                                             new SqlParameter("@DIACHI",  model.DiaChi.Trim()),
+                                                new SqlParameter("@SDT",  model.SDT.Trim()),
+                                                new SqlParameter("@EMAIL",  model.EMAIL.Trim())};
+                DataTable dt = db.GetDataTableFromSP("SP_SUANCC", parameters);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi khi sửa: " + ex.Message);
+            }
+        }
+        public DataTable Delete(string ma)
+        {
+            try
+            {
+                SqlParameter[] parameters = { new SqlParameter("@MANCC", ma) };
+                DataTable dt = db.GetDataTableFromSP("SP_XOANCC", parameters);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Lỗi khi xoá: " + ex.Message);
             }
         }
     }
