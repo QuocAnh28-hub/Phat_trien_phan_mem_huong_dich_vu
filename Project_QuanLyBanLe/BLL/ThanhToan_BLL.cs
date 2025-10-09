5df0362728c237987304d038a84b56755a6d5fdf
Do Tien Dat <dotiendat092005@gmail.com>
2025-10-09 22:58:38 +0700

    update ThanhToan_BLL

--- Project_QuanLyBanLe/BLL/ThanhToan_BLL.cs
+++ Project_QuanLyBanLe/BLL/ThanhToan_BLL.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DAL;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,27 @@ namespace BLL
 {
     public class ThanhToan_BLL
     {
+        ThanhToan_DAL _DAL = new ThanhToan_DAL();
+
+        public DataTable getAll()
+        {
+            return _DAL.getAll();
+        }
+        public DataTable GetById(string ma)
+        {
+            return _DAL.GetById(ma);
+        }
+        public DataTable Delete(string ma)
+        {
+            return _DAL.Delete(ma);
+        }
+        public DataTable Update(Models.ThanhToan model)
+        {
+            return _DAL.Update(model);
+        }
+        public DataTable Create(Models.ThanhToan model)
+        {
+            return _DAL.Create(model);
+        }
     }
 }
