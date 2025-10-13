a6f4dd6a86b8244fb5d5ee4dc61470e5b721dc84
HOANGGLEEE <hle11840@gmail.com>
2025-10-13 11:01:42 +0700

    them APi TaiKhoan,API NhanVien,API ChiTietNhap,API PhieuNhapKho

---
+++ BLL/ChiTietNhap_BLL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using DAL;
+using Models;
+
+namespace BLL
+{
+    public class ChiTietNhap_BLL
+    {
+        ChiTietNhap_DAL CTN_DAL = new ChiTietNhap_DAL();
+
+        public DataTable GetAllChiTietNhap()
+        {
+            return CTN_DAL.GetAllChiTietNhap();
+        }
+
+        public DataTable GetByIdChiTietNhap(string maphieunhap, string masp)
+        {
+            return CTN_DAL.GetByIdChiTietNhap(maphieunhap, masp);
+        }
+
+        public DataTable GetByPhieuNhapKho(string maphieunhap)
+        {
+            return CTN_DAL.GetByPhieuNhapKho(maphieunhap);
+        }
+
+        public DataTable CreateChiTietNhap(ChiTietNhap ctn)
+        {
+            return CTN_DAL.CreateChiTietNhap(ctn);
+        }
+
+        public DataTable UpdateChiTietNhap(ChiTietNhap ctn)
+        {
+            return CTN_DAL.UpdateChiTietNhap(ctn);
+        }
+
+        public DataTable DeleteChiTietNhap(string maphieunhap, string masp)
+        {
+            return CTN_DAL.DeleteChiTietNhap(maphieunhap, masp);
+        }
+    }
+}
---
+++ BLL/TaiKhoan_BLL.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using DAL;
+using Models;
+
+namespace BLL
+{
+    public class TaiKhoan_BLL
+    {
+        TaiKhoan_DAL TK_DAL = new TaiKhoan_DAL();
+
+        public DataTable GetAllTaiKhoan()
+        {
+            return TK_DAL.GetAllTaiKhoan();
+        }
+
+        public DataTable GetByIdTaiKhoan(string mataikhoan)
+        {
+            return TK_DAL.GetByIdTaiKhoan(mataikhoan);
+        }
+
+        public DataTable CreateTaiKhoan(TaiKhoan tk)
+        {
+            return TK_DAL.CreateTaiKhoan(tk);
+        }
+
+        public DataTable UpdateTaiKhoan(TaiKhoan tk)
+        {
+            return TK_DAL.UpdateTaiKhoan(tk);
+        }
+
+        public DataTable DeleteTaiKhoan(string mataikhoan)
+        {
+            return TK_DAL.DeleteTaiKhoan(mataikhoan);
+        }
+
+        public DataTable Login(string username, string password)
+        {
+            return TK_DAL.Login(username, password);
+        }
+    }
+}
