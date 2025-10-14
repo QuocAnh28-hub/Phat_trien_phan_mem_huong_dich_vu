0384de8ba12e8fa0f2549bb8eaec274721e03c71
HOANGGLEEE <hle11840@gmail.com>
2025-10-14 22:55:17 +0700

    cập nhật models

---
+++ Project_QuanLyBanLe/Models/TaiKhoan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class TaiKhoan
+    {
+
+        public string MATAIKHOAN { get; set; }
+        public string USERNAME { get; set; }
+        public string PASS { get; set; }
+        public int QUYEN { get; set; }
+    }
+}
