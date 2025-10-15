2b3777af1c37d5b8b43c4f76943b72d5c357c4be
Quoc_Anh <dohuuquocanh21dk@gmail.com>
2025-10-15 22:59:35 +0700

    Clear các API phụ

--- Project_QuanLyBanLe/API_NhanVien/obj/Debug/net8.0/API_NhanVien.AssemblyInfo.cs
+++ Project_QuanLyBanLe/API_NhanVien/obj/Debug/net8.0/API_NhanVien.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_NhanVien")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c8297c4ac6ac1dd868db3fad792c31d8b432978")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfcf16ed8f9d126f282c66db2e83f8ae652278e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_NhanVien")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_NhanVien")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Project_QuanLyBanLe/BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
+++ Project_QuanLyBanLe/BLL/obj/Debug/net8.0/BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c8297c4ac6ac1dd868db3fad792c31d8b432978")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfcf16ed8f9d126f282c66db2e83f8ae652278e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
